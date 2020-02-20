dd0c81ecd416f00f769170143f8a53c8ae3e6c64
egobiah <hureau.olivier@gmail.com>
2020-02-20 16:46:15 +0100

    TP CHAT HELLO SERVER

---
+++ CHAT/src/Main.java
@@ -0,0 +1,19 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("GO !");
+    }
+
+
+
+}
